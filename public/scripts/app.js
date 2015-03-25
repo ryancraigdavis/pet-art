278a2f9ed0f7620e05661666abcd77adc1b634c3
Ryan <RR.Schweitzer@gmail.com>
2015-03-24 20:52:37 -0600

    Adjusted routing but need to fix navbar class on route changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('petApp',['ui.router']);
+// var app = angular.module('petApp',['ui.router']);
 
 // app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 //     $urlRouterProvider.otherwise('/');
@@ -16,3 +16,25 @@ var app = angular.module('petApp',['ui.router']);
 //         })
  
 // }]);
+
+var app = angular.module('petApp', ['ngRoute'])
+
+	app.config(function($routeProvider) {
+		$routeProvider
+		.when('/', {
+			templateUrl: "homeView.html",
+			controller:  "mainCtrl"
+		})
+		.when('/artists', {
+			templateUrl: 'artists.html',
+			controller: 'artistsCtrl'
+		})
+		.when('/gallery', {
+			templateUrl: 'gallery.html',
+			controller: 'galleryCtrl'
+		})
+
+		.otherwise({
+			redirectTo: "/"
+		})
+	})
