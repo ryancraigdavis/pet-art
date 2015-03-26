e0603abedba0da23a4eba940bf6dc1fea3d2ddeb
Ryan <RR.Schweitzer@gmail.com>
2015-03-26 08:46:40 -0600

    Finished

--- public/scripts/indexCtrl.js
+++ public/scripts/indexCtrl.js
@@ -1,8 +1,17 @@
 var app = angular.module('petApp');
-	app.controller('indexCtrl', function ($scope) {
+	app.controller('indexCtrl', function ($scope, $location, $rootScope) {
 		$(".contact-us").click(function() {
     	$('html, body').animate({
         scrollTop: $("#cta").offset().top
     	}, 1500);
   	});
+
+		$rootScope.$on('$locationChangeSuccess', function(event){
+       var url = $location.url()
+				if (url === "/") {
+					$scope.myVar = "alt";
+				} else {
+					$scope.myVar = "skel-layers-fixed";
+				}		
+		})
 	});
